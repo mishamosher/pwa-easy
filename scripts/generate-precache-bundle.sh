3dd874e8b7b4727d189936816dec6bccaa9aa134
Elmer Miroslav Mosher Golovin <miroslav@mishamosher.com>
2021-02-12 19:32:43 +0300

    Include OpenMoji webfont in the bundle.

--- scripts/generate-precache-bundle.sh
+++ scripts/generate-precache-bundle.sh
@@ -43,8 +43,8 @@ cd ..
     find . -type f -iwholename "./external/css/fontawesome/webfonts/*" -print
     # Roboto webfonts
     find . -type f -iwholename "./external/css/roboto/fonts/*" -print
-    # Openmoji SVGs
-    # find . -type f -iwholename "./external/img/openmoji/*.svg" -print
+    # OpenMoji webfont
+    find . -type f -iwholename "./external/img/openmoji/*.ttf" -print
 
     # Generate precache for all files not under [external|bundle] directories and that don't start with a dot (.)
     # The file service-worker.js is also omitted
